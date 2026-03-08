6b4eb35cb0a7feaa0f7304c3a4c5023b57da7bce
islam fathy <eslamfathyelbasatini55a@gmail.com>
2026-03-08 14:46:13 +0200

    Add project files.

---
+++ DotNetTaskDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace DotNetTaskDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new
+            {
+                Name = "Islam fathy",
+                Age = 22,
+                Skill = ".Net Developer"
+            };
+            string json= JsonConvert.SerializeObject(person);
+            Console.WriteLine(json);
+        }
+    }
+}
